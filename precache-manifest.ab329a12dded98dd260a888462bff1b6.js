0e187705176d48939e9c528d1fa4ea9b48c84343
Nikola Gospodinov <nikola96gospodinov@gmail.com>
2019-04-10 15:07:27 +0100

    Updates

---
+++ precache-manifest.ab329a12dded98dd260a888462bff1b6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb2b7aaf1fb01330ff61",
+    "url": "/PortfolioWebsite/static/css/main.4a9606a8.chunk.css"
+  },
+  {
+    "revision": "eb2b7aaf1fb01330ff61",
+    "url": "/PortfolioWebsite/static/js/main.2e8ffa55.chunk.js"
+  },
+  {
+    "revision": "d7c6a769025bf1063592",
+    "url": "/PortfolioWebsite/static/js/runtime~main.3612083e.js"
+  },
+  {
+    "revision": "09599df4140b251513b6",
+    "url": "/PortfolioWebsite/static/js/2.31d9b65d.chunk.js"
+  },
+  {
+    "revision": "39758cfcb42ab4a93f659dd632613d7b",
+    "url": "/PortfolioWebsite/static/media/logo_transparent.39758cfc.png"
+  },
+  {
+    "revision": "a13d3e4ea75af2c18c74a1f44873db3b",
+    "url": "/PortfolioWebsite/static/media/hero.a13d3e4e.jpg"
+  },
+  {
+    "revision": "797f876b170393987f8e09839a40176d",
+    "url": "/PortfolioWebsite/static/media/heroAbout.797f876b.jpg"
+  },
+  {
+    "revision": "d6e246960d900b51064427fd7db85f4b",
+    "url": "/PortfolioWebsite/static/media/SkillsHero.d6e24696.jpg"
+  },
+  {
+    "revision": "5d09d1188e79207d39004a26f7a9b794",
+    "url": "/PortfolioWebsite/static/media/Story.5d09d118.jpg"
+  },
+  {
+    "revision": "03272cbb98ac1e3a4d4ba14287ac4637",
+    "url": "/PortfolioWebsite/static/media/projectHero.03272cbb.jpg"
+  },
+  {
+    "revision": "60d5935b0e40f110229b3ba7712ebbd2",
+    "url": "/PortfolioWebsite/static/media/nutri.60d5935b.jpg"
+  },
+  {
+    "revision": "f30a28da49889a97a96d4ef60c7bdaef",
+    "url": "/PortfolioWebsite/static/media/THC.f30a28da.jpg"
+  },
+  {
+    "revision": "638b601c6569d918437872e37491ef53",
+    "url": "/PortfolioWebsite/static/media/Portfolio.638b601c.jpg"
+  },
+  {
+    "revision": "d8ccde348f755e5dcfc0b752c815ae27",
+    "url": "/PortfolioWebsite/static/media/aboutMe.d8ccde34.jpg"
+  },
+  {
+    "revision": "071869d4ae06e3e4c044609d9bcf3717",
+    "url": "/PortfolioWebsite/index.html"
+  }
+];
