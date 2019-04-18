476ea7b3d68ffd33944ace9bdd47d71a5d2a6828
nikola96gospodinov <49476990+nikola96gospodinov@users.noreply.github.com>
2019-04-18 16:45:01 +0100

    Add files via upload

--- src/components/pages/Home/Parts/Skills.js
+++ src/components/pages/Home/Parts/Skills.js
@@ -20,17 +20,8 @@ const Skills = () => {
                     <h1><i>Skills</i> - a boring list</h1>
                     <h2>If you want just a simple list of the skills that I have that you can easily download and show to your boss or keep as a reference then this is the right section for you! </h2>
                     <Link
+                        className = "boringSkillsHome"
                         to = "/skills/boring"
-                        style = {{
-                            color: '#333',
-                            border: '2px solid #333',
-                            padding: '1rem 2rem',
-                            fontSize: '1.3rem',
-                            fontWeight: '600',
-                            marginBottom: '2rem',
-                            display: 'inline-block',
-                            cursor: 'pointer'
-                        }}
                     >Get the List</Link>
                 </div>
             </div>
@@ -45,17 +36,8 @@ const Skills = () => {
                     <h1><i>Skills</i> - the unboring stroy</h1>
                     <h2>If you don't want just a boring list of the skills that I have but want to read the story of my journey towards acquiring my skills then this is the right section for you!</h2>
                     <Link
+                        className = "unboringSkillsHome"
                         to = "/skills/unboring"
-                        style = {{
-                            color: '#ccc',
-                            border: '2px solid #ccc',
-                            padding: '1rem 2rem',
-                            fontSize: '1.3rem',
-                            fontWeight: '600',
-                            marginBottom: '2rem',
-                            display: 'inline-block',
-                            cursor: 'pointer'
-                        }}
                     >Discover the Story</Link>
                 </div>
             </div>
